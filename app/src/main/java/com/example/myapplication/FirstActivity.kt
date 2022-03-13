b6422ef1a0d8546b80780ede97019af08869915a
songhaibei <songhaibei7305@163.com>
2022-03-13 20:36:24 +0800

    添加Toast

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.firs_layout)
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+        }
     }
 }
