1bce0126f5ba03ae545cba1867f2af6f2fa905c3
songhaibei <songhaibei7305@163.com>
2022-03-13 23:03:27 +0800

    添加菜单逻辑

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +19,15 @@ class FirstActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT)
+                .show()
+        }
         return true
     }
 }
