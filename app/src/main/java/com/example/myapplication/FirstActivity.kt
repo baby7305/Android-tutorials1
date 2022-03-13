0b22113a3c1dbebef20ef09fdd40f34acfb1fabc
songhaibei <songhaibei7305@163.com>
2022-03-13 20:57:05 +0800

    简化写法
    deprecated

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -2,14 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.firs_layout.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.firs_layout)
-        val button1: Button = findViewById(R.id.button1)
         button1.setOnClickListener {
             Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
         }
