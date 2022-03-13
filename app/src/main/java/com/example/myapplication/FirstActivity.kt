9c5c883f89282353f11ac1c7e6606b10049156c6
songhaibei <songhaibei7305@163.com>
2022-03-13 22:56:18 +0800

    调整导包顺序

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -1,10 +1,10 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.view.Menu
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
