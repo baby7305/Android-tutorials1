d3df34455d5da4aa48b66a5663373bed96f4bc65
songhaibei <songhaibei7305@163.com>
2022-03-14 21:29:06 +0800

    准备日志

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -3,12 +3,14 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.firs_layout)
+        Log.d("FirstActivity", "Task id is $taskId")
         val button1: Button = findViewById(R.id.button1)
         button1.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -3,12 +3,14 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_layout)
+        Log.d("SecondActivity", "Task id is $taskId")
         val button2: Button = findViewById(R.id.button2)
         button2.setOnClickListener {
             val intent = Intent(this, ThirdActivity::class.java)
