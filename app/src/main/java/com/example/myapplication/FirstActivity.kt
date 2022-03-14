d3fec138e4ed408e52184ab96d4e2859e2030f5a
songhaibei <songhaibei7305@163.com>
2022-03-14 20:12:51 +0800

    准备日志

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -3,11 +3,13 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("FirstActivity", this.toString())
         setContentView(R.layout.firs_layout)
         val button1: Button = findViewById(R.id.button1)
         button1.setOnClickListener {
