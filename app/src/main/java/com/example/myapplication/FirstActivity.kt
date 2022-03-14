1610d88d7eb5e2474da6bd9be16d97e5961284cf
songhaibei <songhaibei7305@163.com>
2022-03-14 20:11:38 +0800

    Activity切换

--- app/src/main/java/com/example/myapplication/FirstActivity.kt
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.firs_layout)
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            val intent = Intent(this, FirstActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
