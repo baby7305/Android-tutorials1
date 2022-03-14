2e8bbc3af8cbe44d2c3aac19ac7ff8ad5f6566ae
songhaibei <songhaibei7305@163.com>
2022-03-14 20:30:49 +0800

    Activity切换

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_layout)
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this, FirstActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
