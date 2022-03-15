b2e2b57e4ac3360f82b4877b250ee0e4880bac18
songhaibei <songhaibei7305@163.com>
2022-03-15 12:52:46 +0800

    ProgressBar结合Button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
         button.setOnClickListener {
+            if (progressBar.visibility == View.VISIBLE) {
+                progressBar.visibility = View.GONE
+            } else {
+                progressBar.visibility = View.VISIBLE
+            }
         }
     }
 }
