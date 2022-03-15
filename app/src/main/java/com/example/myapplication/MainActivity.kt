6de340b144fab856dfbe24d3a355a21959a62528
songhaibei <songhaibei7305@163.com>
2022-03-15 11:51:56 +0800

    EditText结合Button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button)
+        val editText: EditText = findViewById(R.id.editText)
         button.setOnClickListener {
+            val inputText = editText.text.toString()
+            Toast.makeText(this, inputText, Toast.LENGTH_SHORT).show()
         }
     }
 }
