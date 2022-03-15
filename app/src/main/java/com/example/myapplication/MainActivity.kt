2a6f1968c356492753f1598fc35042d682a4dd63
songhaibei <songhaibei7305@163.com>
2022-03-15 13:03:58 +0800

    Button修改进度条

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
         button.setOnClickListener {
+            progressBar.progress = progressBar.progress + 10
         }
     }
 }
