23812f93d896e352195bf2b8b0ec6b740a3d3395
songhaibei <songhaibei7305@163.com>
2022-03-15 12:01:17 +0800

    ImageVIew结合Button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +15,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+        val imageView: ImageView = findViewById(R.id.imageView)
         when (v?.id) {
             R.id.button -> {
+                imageView.setImageResource(R.drawable.img_2)
             }
         }
     }
