f07ea9f57f33daa039795058e9388c0613f57cf0
songhaibei <songhaibei7305@163.com>
2022-03-15 13:12:32 +0800

    AlertDialog

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
+            val dialog = AlertDialog.Builder(this).run {
+                setTitle("This is Dialog")
+                setMessage("Something important.")
+                setCancelable(false)
+                setPositiveButton("OK") { dialog, which ->
+                }
+                setNegativeButton("Cancel") { dialog, which ->
+                }
+                create()
+            }
+            dialog.show()
         }
     }
 }
