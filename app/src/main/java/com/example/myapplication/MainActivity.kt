f615a59cdcfe685ad179f43c576c9e1d3b2d5f36
songhaibei <songhaibei7305@163.com>
2022-03-23 23:05:41 +0800

    ListView的简单用法

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    private val data = listOf(
+        "Apple",
+        "Orange",
+        "Watermelon",
+        "Pear",
+        "Grape",
+        "Pineapple",
+        "Strawberry",
+        "Cherry",
+        "Mango",
+        "Apple",
+        "Banana",
+        "Orange",
+        "Watermelon",
+        "Pear",
+        "Grape",
+        "Pineapple",
+        "Strawberry",
+        "Cherry",
+        "Mango"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        val listView: ListView = findViewById(R.id.listView)
+        listView.adapter = adapter
     }
 }
