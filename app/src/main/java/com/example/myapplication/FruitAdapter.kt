13bfa6f483ab06a4d84c6ecbb13fead2a3898a48
songhaibei <songhaibei7305@163.com>
2022-03-24 20:57:52 +0800

    布局缓存

--- app/src/main/java/com/example/myapplication/FruitAdapter.kt
+++ app/src/main/java/com/example/myapplication/FruitAdapter.kt
@@ -11,7 +11,12 @@ import android.widget.TextView
 class FruitAdapter(activity: Activity, val resourceId: Int, data: List<Fruit>) :
     ArrayAdapter<Fruit>(activity, resourceId, data) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+        val view: View
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+        } else {
+            view = convertView
+        }
         val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
         val fruitName: TextView = view.findViewById(R.id.fruitName)
         val fruit = getItem(position) // 获取当前项的Fruit实例
